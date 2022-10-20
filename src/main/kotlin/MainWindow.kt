f9ea059e4706b823eb7187f792b452cbcdec33d2
max-sliva <persimon@inbox.ru>
2022-10-20 18:29:41 +0500

    done making gif

--- src/main/kotlin/MainWindow.kt
+++ src/main/kotlin/MainWindow.kt
@@ -16,11 +16,11 @@ import javafx.scene.layout.StackPane
 import javafx.stage.FileChooser
 import javafx.stage.FileChooser.ExtensionFilter
 import java.io.File
+import java.nio.file.Files
 import java.nio.file.Paths
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.imageio.ImageIO
-
 
 class MainWindow {
     @FXML lateinit var mainPane: BorderPane //main window
@@ -32,25 +32,26 @@ class MainWindow {
     @FXML lateinit var gifBtn: Button
     var isMakingGIF = false
     val fileToImageViewMap: HashMap<String, ImageView> = HashMap()
+    var dirPath: String = ""
+    var imgCount = 0
     fun onAddImageClick(actionEvent: ActionEvent) {
         opacitySlider.value = 100.0
         println("Add image")
         println(mainPane.scene.window)
-        val fileChooser = FileChooser()
-        val currentPath: String = Paths.get(".").toAbsolutePath().normalize().toString()
-        fileChooser.initialDirectory = File(currentPath);
-        fileChooser.title = "Open Image File"
-        fileChooser.extensionFilters.addAll(
-            ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.tif"),
-            ExtensionFilter("All Files", "*.*")
-        )
+        val fileChooser = FileChooser().apply{
+            title = "Open Image File"
+            val currentPath: String = Paths.get(".").toAbsolutePath().normalize().toString()
+            initialDirectory = File(currentPath)
+            extensionFilters.addAll(ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                                    ExtensionFilter("All Files", "*.*"))
+        }
         val file = fileChooser.showOpenDialog(mainPane.scene.window)
-
         if (file!=null) {
             println("file = ${file.path}")
             val imageView = ImageView()
             imageView.image = Image("file:${file.path}")
             imageView.isPreserveRatio = true
+            imageView.fitHeight = stackPaneWithImages.height
 //            imageView.fitWidthProperty().bind(mainPane.widthProperty())
 //            imageView.fitHeightProperty().bind(mainPane.heightProperty())
             stackPaneWithImages.children.add(imageView)
@@ -67,17 +68,21 @@ class MainWindow {
             opacitySlider.value = fileToImageViewMap[imagePicker.value]!!.opacity*100
         }
 
-        opacitySlider.valueProperty().addListener{ _, _, newVal ->
-            println("slider value = $newVal")
+        opacitySlider.valueProperty().addListener{ _, oldVal, newVal ->
+//            println("slider oldValue = $oldVal")
+//            println("slider newValue = $newVal")
             fileToImageViewMap[imagePicker.value]!!.opacity = newVal.toInt() / 100.0
+            if (isMakingGIF){
+                if (oldVal.toInt()==100) makeImage("png")
+                if (newVal.toInt()%5==0) makeImage("png")
+            }
         }
 
-        buttomSlider.valueProperty().addListener { _, _, newVal ->
+        buttomSlider.valueProperty().addListener { _, _, newVal ->  //делаем видимой часть картинки в зависимости от положения нижнего бегунка
             val curX = (fileToImageViewMap[imagePicker.value]!!.image.width * newVal.toInt()) / 100
             fileToImageViewMap[imagePicker.value]!!.viewport = Rectangle2D(curX, 0.0, fileToImageViewMap[imagePicker.value]!!.image.width,
                                                                                                         fileToImageViewMap[imagePicker.value]!!.image.height)
-            fileToImageViewMap[imagePicker.value]!!.translateX = curX * fileToImageViewMap[imagePicker.value]!!.scaleX
-
+            fileToImageViewMap[imagePicker.value]!!.translateX = curX * fileToImageViewMap[imagePicker.value]!!.scaleX //и сдвигаем картинку на нужное значение
         }
     }
 
@@ -111,27 +116,41 @@ class MainWindow {
         }
     }
 
-    fun makeGif(actionEvent: ActionEvent) {
+    fun makeImage(type: String) { //make thread for this work
         val snapshot: WritableImage = stackPaneWithImages.snapshot(SnapshotParameters(), null)
-        val date = Calendar.DATE
-
-        val file = File("img.png")
-        ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null),"png", file);
+        val file = File("$dirPath/img$imgCount.$type")
+        imgCount++
+        ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null),"$type", file)
 //        val image = (stackPaneWithImages as ImageView).image
 //        image.
     }
 
     fun gifBtnClick(actionEvent: ActionEvent) {
         isMakingGIF = !isMakingGIF
-        if (gifBtn.text =="stop gif") writeGif()
+        if (gifBtn.text =="start gif") beginGifWork()
+        if (gifBtn.text =="stop gif") {
+            val gifWork = GifWork(dirPath)
+            gifWork.makeGif()
+        }
         gifBtn.text = if (isMakingGIF) "stop gif" else "start gif"
     }
 
-    private fun writeGif() {
-        println("gif created!")
+    private fun beginGifWork() {
+        println("gif work has begun!")
+        imgCount = 0
         val rightNow = Calendar.getInstance()
         val date = rightNow.time as Date
-        val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm_ss")
+        val sdf = SimpleDateFormat("dd_MM_yy HH_mm_ss")
+        dirPath = sdf.format(date)
+//        val currentPath: String = Paths.get(".").toAbsolutePath().normalize().toString()
         println("date = ${sdf.format(date)}")
+        Files.createDirectories(Paths.get("$dirPath"))
+    }
+
+    fun oneGif(actionEvent: ActionEvent) {
+        if (isMakingGIF){
+           makeImage("png")
+        }
+
     }
 }
