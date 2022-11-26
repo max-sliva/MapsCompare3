4987a3ab638ba63871b96b1f260000681ab43f1b
max-sliva <persimon@inbox.ru>
2022-11-26 12:32:25 +0500

    done scaling for cropping in swing

--- src/main/kotlin/TiffWork.kt
+++ src/main/kotlin/TiffWork.kt
@@ -103,7 +103,7 @@ fun main(){
             square.size = Dimension(e!!.x-dragStartX, e!!.y-dragStartY)
             //при рисовании прямоугольника сначала вставляем картинку размером с вырезанный до этого участок, потом сам прямоугольник
             //чтобы не было мерцания
-            grForLabel2d?.drawImage(cropImage(image as BufferedImage, square.x-3, square.y-3, square.width+3, square.height+3), null, square.x+2, square.y+2)
+            grForLabel2d?.drawImage(cropImage(image as BufferedImage, square.x-3, square.y-3, square.width+3, square.height+3, imageScale), null, square.x+2, square.y+2)
             grForLabel2d?.draw(square)
             super.mouseDragged(e)
         }
@@ -115,6 +115,7 @@ fun main(){
 //            imgLabel?.icon = imgIcon
             square.size = Dimension(e!!.x-dragStartX, e!!.y-dragStartY)
             grForLabel2d?.draw(square)
+            grForLabel2d?.dispose()
             inDrawingRect = false
         }
     }
@@ -130,8 +131,9 @@ fun main(){
             image = ImageIO.read(file)
             println("file is read")
             println("img width = ${image?.width}  height = ${image?.height}")
-            val newWidth = image?.width!!
+            val newWidth = 1400
             imageScale = image?.width!! / newWidth.toDouble()
+//            imageScale = newWidth.toDouble() / image?.width!!
             imgIcon.image = image?.getScaledInstance(newWidth, -1, Image.SCALE_FAST)
             println("img is scaled, scale = $imageScale")
             println("img width = ${image?.width}  height = ${image?.height}")
@@ -154,6 +156,8 @@ fun main(){
     val zoomPlusBtn = JButton("zoom +")
     zoomPlusBtn.addActionListener {
         imageScale = image?.width!! / (imgIcon.iconWidth.toDouble()+100)
+//        imageScale = (imgIcon.iconWidth.toDouble()+100) / image?.width!!
+
         imgIcon.image = image?.getScaledInstance(imgIcon.iconWidth+100, -1, Image.SCALE_FAST)
         println("img is scaled, scale = $imageScale")
         imgLabel?.icon = null
@@ -166,6 +170,7 @@ fun main(){
     val zoomMinusBtn = JButton("zoom -")
     zoomMinusBtn.addActionListener {
         imageScale = image?.width!! / (imgIcon.iconWidth.toDouble()-100)
+//        imageScale = (imgIcon.iconWidth.toDouble()+100) / image?.width!!
         imgIcon.image = image?.getScaledInstance(imgIcon.iconWidth-100, -1, Image.SCALE_FAST)
         println("img is scaled, scale = $imageScale")
         imgLabel?.icon = null
@@ -176,11 +181,13 @@ fun main(){
     southBox.add(zoomMinusBtn)
     southBox.add(Box.createHorizontalGlue())
     cropBtn.addActionListener {
-        val image = cropImage(image as BufferedImage, square.x, square.y, square.width, square.height)
+        val image = cropImage(image as BufferedImage, square.x, square.y, square.width, square.height, imageScale)
         imgIcon.image = image
         imgLabel?.icon = null
         imgLabel?.icon = imgIcon
     }
+
+    //todo add button for scaling image to the size of window
     cropBtn.isEnabled = false
     southBox.add(cropBtn)
     southBox.add(Box.createHorizontalGlue())
@@ -200,7 +207,21 @@ fun readTiffFromFile(myFrame: JFrame): File {
     return File(fileName)
 }
 
-fun cropImage(originalImgage: BufferedImage, x: Int, y: Int, w: Int, h: Int): BufferedImage? {
-    //todo при кропе проверять scale и менять параметры выреза
-    return originalImgage.getSubimage(x, y, w, h)
+fun cropImage(originalImgage: BufferedImage, x: Int, y: Int, w: Int, h: Int, imageScale: Double = 1.0): BufferedImage? {
+    var croppedImage = originalImgage.getSubimage((x*imageScale).toInt(), (y*imageScale).toInt(), (w*imageScale).toInt(), (h*imageScale).toInt())
+    croppedImage = croppedImage?.getScaledInstance((croppedImage.width/imageScale).toInt(), -1, Image.SCALE_FAST)?.toBufferedImage()
+    return croppedImage
+}
+
+fun Image.toBufferedImage(): BufferedImage {
+    if (this is BufferedImage) {
+        return this
+    }
+    val bufferedImage = BufferedImage(this.getWidth(null), this.getHeight(null), BufferedImage.TYPE_INT_ARGB)
+
+    val graphics2D = bufferedImage.createGraphics()
+    graphics2D.drawImage(this, 0, 0, null)
+    graphics2D.dispose()
+
+    return bufferedImage
 }
